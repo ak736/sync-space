ff572127eed92fc910ae299d01de3222e0abe0fc
ak736 <as0700@srmist.edu.in>
2024-05-04 20:27:54 +0530

    fix bugs

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -5,7 +5,6 @@ import { cookies } from 'next/headers';
 export async function GET(req: NextRequest) {
   const requestUrl = new URL(req.url);
   const code = requestUrl.searchParams.get('code');
-
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
